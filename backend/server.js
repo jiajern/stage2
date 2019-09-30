06419e7e25502bf0e8e5e964dae94da4a5116277
Alan Lau <alanlau97@gmail.com>
2019-09-29 21:02:08 -0400

    initial setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Connected to the DB");
+});
+
+app.listen(port, () => {
+    console.log(`Connected to the server at http://localhost:${port}`);
+});
