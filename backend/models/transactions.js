d6376629fac17a1645b1d59ebc4eaf80fc7abe29
Alan Lau <alanlau97@gmail.com>
2019-10-03 23:22:01 -0400

    update model stock and transaction

---
+++ backend/models/transactions.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+var Transaction = new Schema({
+    stock:{
+        type: String,
+        required: true
+    },
+    quantity:{
+        type: Number,
+        min: 1,
+        required: true
+    },
+    is_sell:{
+        type: Boolean,
+        required: true
+    },
+    trader:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Transaction', Transaction);
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,7 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
 
+var Stock = new Schema({
+    symbol:{
+        type: String,
+        required: true
+    },
+    quantity:{
+        type: Number,
+        require: true
+    },
+    price:{
+        type: Currency,
+        require: true
+    }
+}, {
+    timestamps: true
+});
 
 var User = new Schema({
     email: {
@@ -17,10 +35,18 @@ var User = new Schema({
         type: String,
         default: ''
     },
+    balance: {
+        type: Currency,
+        default: 5000,
+        min: 0
+    },
+    own_stock: [Stock],
     verified: {
         type: Boolean,
         default: false
     }
+}, {
+    timestamps: true
 });
 User.plugin(passportLocalMongoose, {usernameField:"email"});
 
