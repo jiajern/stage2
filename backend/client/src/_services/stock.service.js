ff8d0098b03efbcd9c8efb22e2465910c1f3d020
Alan Lau <alanlau97@gmail.com>
2019-10-06 18:37:07 -0400

    able to get stock data

--- backend/client/src/_services/stock.service.js
+++ backend/client/src/_services/stock.service.js
@@ -1,5 +1,5 @@
 import config from 'config';
-import { handleResponse } from '@/_helpers';
+import { handleResponse, authHeader } from '@/_helpers';
 
 export const stockService = {
     getStocks
@@ -7,6 +7,6 @@ export const stockService = {
 
 function getStocks(symbols) {
     const symbolString = '?symbol='.concat(symbols.join());
-    const requestOptions = { method: 'GET'};
-    return fetch(apiUrl.concat('/stock',symbolString), requestOptions).then(handleResponse);
+    const requestOptions = { method: 'GET', headers: authHeader()};
+    return fetch(config.apiUrl.concat('/stock',symbolString), requestOptions).then(handleResponse);
 }
--- backend/routes/transRoute.js
+++ backend/routes/transRoute.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Users = require('../models/users');
 const transRouter = express.Router();
+const request = require('request');
 var auth = require('../middleware/auth');
+
 require('dotenv').config();
 //stockApiUrl: 'https://api.worldtradingdata.com/api/v1/stock'
 transRouter.use(bodyParser.json());
@@ -11,9 +13,13 @@ transRouter.get('/stock', auth.verifyUser, (req, res, next) => {
     let symbol = req.query.symbol;
     let key = process.env.API_KEY;
     let api = 'https://api.worldtradingdata.com/api/v1/stock'.concat('?symbol=', symbol, '&api_token=', key);
-    
+    request(api, (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred and handle it
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        res.send(body);
+    });
 
-});
+})
 // post
 // buy new stock
 transRouter.post('/stock/:userId', auth.verifyUser, (req, res, next) => {
