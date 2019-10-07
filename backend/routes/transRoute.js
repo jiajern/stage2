5104b42867eef26a5c179bed33a8dd6136f45e84
Alan Lau <alanlau97@gmail.com>
2019-10-06 21:25:55 -0400

    buy stock

--- backend/routes/transRoute.js
+++ backend/routes/transRoute.js
@@ -4,6 +4,7 @@ const Users = require('../models/users');
 const transRouter = express.Router();
 const request = require('request');
 var auth = require('../middleware/auth');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 //stockApiUrl: 'https://api.worldtradingdata.com/api/v1/stock'
@@ -21,9 +22,75 @@ transRouter.get('/stock', auth.verifyUser, (req, res, next) => {
 
 })
 // post
-// buy new stock
+// buy stock
 transRouter.post('/stock/:userId', auth.verifyUser, (req, res, next) => {
-    
+    var userId = req.params.userId;
+    var currentUser = req.headers;
+    var token = currentUser.authorization.split(" ")[1];
+    currentUser = jwt.decode(token)._id;
+    if (currentUser !== userId) {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ status: 403, message: "Not allowed" });
+    }
+    // find the user
+    let stocks = []
+    let isNewStock = true;
+    Users.findById(req.params.userId)
+    .then((user) => {
+        if(user.balance < req.body.quantity * req.body.price) {
+            res.statusCode = 403;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ status: 403, message: "Not allowed" });
+        }
+        stocks = user.own_stock;
+        for(let i = 0; i < stocks.length; i++){
+            if(stocks[i].symbol === req.body.symbol) {
+                isNewStock = false;
+                let old_total = stocks[i].quantity * stocks[i].price;
+                let new_quantity = stocks[i].quantity + req.body.quantity;
+                let new_price = (req.body.quantity * req.body.price + old_total) / new_quantity;
+                stocks[i] = {
+                    symbol: stocks[i],
+                    quantity: new_quantity,
+                    price: new_price
+                }     
+            }
+        }
+        let update = {};
+        let newBalance = user.balance - (req.body.quantity * req.body.price);
+        let newTrans = user.transaction
+        newTrans.push({
+            stock:req.body.symbol,
+            price:req.body.price,
+            quantity:req.body.quantity,
+            is_sell:false
+        });
+        if (isNewStock) {
+            stocks.push({
+                symbol: req.body.symbol,
+                quantity: req.body.quantity,
+                price: req.body.price
+            });
+        }
+        update = { own_stock: stocks, balance: newBalance, transaction: newTrans };
+        console.log(update);
+        // do the update
+         return Users.findByIdAndUpdate(userId, {
+            $set: update
+        }, {new: true});
+    })
+    .then((user) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(user)
+    })
+    .catch((err) => {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: err });
+    })
+    // need the symbol, price, quantity
 });
 // TODO
 // update
